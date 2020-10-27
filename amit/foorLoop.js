b6d882a4a4f52e8bcded94be3357f65d657ad44c
Amit833 <amitmajumder833@gmail.com>
2020-10-27 16:24:05 +0100

    Please check my task-Amt

---
+++ amit/foorLoop.js
@@ -0,0 +1,9 @@
+let number = 15;
+
+if (number % 3 === 0) {
+  console.log("Fizz");
+} else if (number % 5 === 0) {
+  console.log("Buzz");
+} else if (number % 3 && 5 === 0) {
+  console.log("FizzBuzz");
+}
